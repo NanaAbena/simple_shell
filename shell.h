6da9b12c5de301a42638e52a385c8ad505ae1d93
NanaAbena <nanykyeraa@gmail.com>
2023-06-26 14:59:17 -0700

    Shell header file created

---
+++ shell.h
@@ -0,0 +1,15 @@
+#ifndef SHELL_HEADER_FILE
+#define SHELL_HEADER_FILE
+
+#include <unistd.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+
+int main(void);
+
+#endif
+
